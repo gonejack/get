6a6682ea3f4c4a08b9226bee950c4c12b2da531c
gonejack <igonejack@gmail.com>
2021-08-17 17:35:39 +0800

    update dependency
    Signed-off-by: gonejack <igonejack@gmail.com>

--- get.go
+++ get.go
@@ -3,15 +3,13 @@ package get
 import (
 	"context"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"path/filepath"
 	"sync"
 	"time"
 
 	"github.com/dustin/go-humanize"
-	"github.com/schollz/progressbar/v3"
+	"github.com/go-resty/resty/v2"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
 )
@@ -19,15 +17,14 @@ import (
 type Getter struct {
 	BeforeDL func(ref string, path string)
 	AfterDL  func(ref string, path string, err error)
-
-	Header  map[string]string
-	Client  http.Client
-	Verbose bool
+	Header   map[string]string
+	Client   http.Client
 }
 
 func (g *Getter) Download(ref string, path string, timeout time.Duration) (err error) {
 	ctx, cancel := context.WithTimeout(context.TODO(), timeout)
 	defer cancel()
+
 	return g.DownloadWithContext(ctx, ref, path)
 }
 func (g *Getter) DownloadWithContext(ctx context.Context, ref string, path string) (err error) {
@@ -35,49 +32,27 @@ func (g *Getter) DownloadWithContext(ctx context.Context, ref string, path strin
 		g.BeforeDL(ref, path)
 	}
 	if g.AfterDL != nil {
-		defer func() { g.AfterDL(ref, path, err) }()
+		defer func() {
+			g.AfterDL(ref, path, err)
+		}()
 	}
-
 	if g.shouldSkip(ctx, ref, path) {
 		return
 	}
 
-	file, err := os.Create(path)
-	if err != nil {
-		return
-	}
-	defer file.Close()
+	req := resty.NewWithClient(&g.Client).R()
+	req.SetContext(ctx)
+	req.SetHeaders(g.Header)
+	req.SetOutput(path)
 
-	request, err := http.NewRequestWithContext(ctx, http.MethodGet, ref, nil)
-	if err != nil {
-		return
-	}
-	resp, err := g.Client.Do(g.patchHeader(request))
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return fmt.Errorf("response status code %d invalid", resp.StatusCode)
-	}
-
-	var writer io.Writer = file
-	if g.Verbose {
-		bar := g.progressBar(ref, resp.ContentLength)
-		defer func() {
-			_ = bar.Clear()
-			_ = bar.Close()
-		}()
-		writer = io.MultiWriter(file, bar)
-	}
-
-	wrote, err := io.Copy(writer, resp.Body)
+	rsp, err := req.Get(ref)
 	switch {
 	case err != nil:
 		return
-	case wrote < resp.ContentLength:
-		return fmt.Errorf("expected %s but downloaded %s", humanize.Bytes(uint64(resp.ContentLength)), humanize.Bytes(uint64(wrote)))
+	case !rsp.IsSuccess():
+		return fmt.Errorf("response status code %d invalid", rsp.StatusCode())
+	case rsp.Size() < rsp.RawResponse.ContentLength:
+		return fmt.Errorf("expected %s but downloaded %s", humanize.Bytes(uint64(rsp.RawResponse.ContentLength)), humanize.Bytes(uint64(rsp.Size())))
 	default:
 		f, e := os.OpenFile(path+".ok", os.O_RDWR|os.O_CREATE, 0666)
 		if e == nil {
@@ -104,30 +79,30 @@ func (g *Getter) Batch(downloads map[string]string, concurrent int, eachTimeout 
 	return
 }
 func (g *Getter) BatchInOrder(refs []string, paths []string, concurrent int, eachTimeout time.Duration) (errRefs []string, errors []error) {
-	var batch = semaphore.NewWeighted(int64(concurrent))
-	var group errgroup.Group
-	var mutex sync.Mutex
+	var w = semaphore.NewWeighted(int64(concurrent))
+	var eg errgroup.Group
+	var mu sync.Mutex
 
 	for i := range refs {
-		_ = batch.Acquire(context.TODO(), 1)
+		_ = w.Acquire(context.TODO(), 1)
 
 		ref, path := refs[i], paths[i]
-		group.Go(func() (err error) {
-			defer batch.Release(1)
+		eg.Go(func() (e error) {
+			defer w.Release(1)
 
-			err = g.Download(ref, path, eachTimeout)
-			if err != nil {
-				mutex.Lock()
+			e = g.Download(ref, path, eachTimeout)
+			if e != nil {
+				mu.Lock()
 				errRefs = append(errRefs, ref)
-				errors = append(errors, err)
-				mutex.Unlock()
+				errors = append(errors, e)
+				mu.Unlock()
 			}
 
 			return
 		})
 	}
 
-	_ = group.Wait()
+	_ = eg.Wait()
 
 	return
 }
@@ -147,36 +122,13 @@ func (g *Getter) shouldSkip(ctx context.Context, ref string, path string) (skip 
 		return
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodHead, ref, nil)
-	if err != nil {
-		return
-	}
-	resp, err := g.Client.Do(g.patchHeader(req))
-	if err != nil {
-		return
-	}
-	if stat.Size() == resp.ContentLength {
+	rq := resty.NewWithClient(&g.Client).R()
+	rq.SetContext(ctx)
+	rq.SetHeaders(g.Header)
+	rp, err := rq.Head(ref)
+	if err == nil && stat.Size() == rp.RawResponse.ContentLength {
 		return true // skip download
 	}
 
 	return
-}
-func (g *Getter) patchHeader(request *http.Request) *http.Request {
-	for head, content := range g.Header {
-		request.Header.Set(head, content)
-	}
-	return request
-}
-func (g *Getter) progressBar(ref string, size int64) *progressbar.ProgressBar {
-	return progressbar.NewOptions64(size,
-		progressbar.OptionSetTheme(progressbar.Theme{Saucer: "=", SaucerPadding: ".", BarStart: "|", BarEnd: "|"}),
-		progressbar.OptionSetWidth(10),
-		progressbar.OptionSpinnerType(11),
-		progressbar.OptionShowBytes(true),
-		progressbar.OptionShowCount(),
-		progressbar.OptionSetPredictTime(false),
-		progressbar.OptionSetDescription(filepath.Base(ref)),
-		progressbar.OptionSetRenderBlankState(true),
-		progressbar.OptionClearOnFinish(),
-	)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/dustin/go-humanize v1.0.0
-	github.com/schollz/progressbar/v3 v3.8.0
+	github.com/go-resty/resty/v2 v2.6.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
